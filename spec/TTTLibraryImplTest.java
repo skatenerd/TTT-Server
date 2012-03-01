a1cd974af63f2fdc7d8642574b949a5f30aac377
Wai Lee Chin Feman <wchinfeman@8thlight.com>
2012-03-01 11:26:34 -0600

    forgot to add file

---
+++ spec/TTTLibraryImplTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ * Created by IntelliJ IDEA.
+ * User: 8thlight
+ * Date: 3/1/12
+ * Time: 10:23 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TTTLibraryImplTest {
+    @Test
+    public void getsEasyWin(){
+        String board="oo xxoxx ";
+        char player='o';
+        int[] intendedMove={0,2};
+        TTTLibrary tttLibrary=new TTTLibraryImpl();
+        assertArrayEquals(intendedMove, tttLibrary.getMove(board, player));
+        
+    }
+    
+    
+}
