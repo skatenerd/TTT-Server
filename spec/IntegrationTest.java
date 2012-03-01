dd63cf820f7cac74b70cae07a031836eaac643ca
Wai Lee Chin Feman <wchinfeman@8thlight.com>
2012-03-01 10:56:14 -0600

    send good request, get response, yay

--- spec/IntegrationTest.java
+++ spec/IntegrationTest.java
@@ -14,7 +14,7 @@ import static org.junit.Assert.*;
  * To change this template use File | Settings | File Templates.
  */
 public class IntegrationTest {
-    int port=9001;
+    int port=8080;
     
     private Runnable startServer=new Runnable() {
         @Override
@@ -24,32 +24,33 @@ public class IntegrationTest {
         }
     };
     
-//    @Test
-//    public void buildsTTTResponse()
-//    throws Exception{
-//        new Thread(startServer).start();
-//        Thread.sleep(400);
-//        Socket socket=new Socket("localhost",port);
-//        String board="xoxo xo x";
-//        char player='x';
-//        String body=("board="+board+"&player="+player);
-//        String contentLength=Integer.toString(body.getBytes().length);
-//        byte [] requestBytes=("POST /ttt HTTP/1.1\nContent-Length: "+contentLength+"\n\n"+body).getBytes();
-//
-//        socket.getOutputStream().write(requestBytes);
-//
-//        BufferedReader reader=new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//        String line="";
-//        Thread.sleep(50);
-//        while(reader.ready()){
-//            line=reader.readLine();
-//        }
-//
-//        int [] move=new TTTLibraryImpl().getMove(board,player);
-//        String moveString=Integer.toString(move[0])+Integer.toString(move[1]);
-//        assertTrue(line.indexOf(moveString)>0);
-//
-//    }
+    @Test
+    public void buildsTTTResponse()
+    throws Exception{
+        new Thread(startServer).start();
+        Thread.sleep(400);
+        Socket socket=new Socket("localhost",port);
+        String board="xoxxox   ";
+        char player='o';
+        String body=("board="+board+"&player="+player);
+        String contentLength=Integer.toString(body.getBytes().length);
+        byte [] requestBytes=("POST /ttt HTTP/1.1\nContent-Length: "+contentLength+"\n\n"+body).getBytes();
+
+        socket.getOutputStream().write(requestBytes);
+
+        BufferedReader reader=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String line="";
+        Thread.sleep(2000);
+        while(reader.ready()){
+            line=reader.readLine();
+            System.out.println(line);
+        }
+
+        int [] move=new TTTLibraryImpl().getMove(board,player);
+        String moveString=Integer.toString(move[0])+Integer.toString(move[1]);
+        assertTrue(line.indexOf(moveString)>0);
+
+    }
     
     
 }
