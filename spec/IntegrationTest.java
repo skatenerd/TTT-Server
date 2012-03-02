1f66b87d9a1a50955dd8917410867c4ce366df23
Wai Lee Chin Feman <wchinfeman@8thlight.com>
2012-03-02 11:28:24 -0600

    integration test added.  points out that TTTLIbraryImpl is not done

--- spec/IntegrationTest.java
+++ spec/IntegrationTest.java
@@ -1,9 +1,13 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 /**
@@ -15,20 +19,26 @@ import static org.junit.Assert.*;
  */
 public class IntegrationTest {
     int port=8080;
+    Server _server;
+
+    @Before
+    public void startServer()
+    throws Exception{
+        List<ResponseSubsystem>subsystems=new ArrayList<ResponseSubsystem>();
+        subsystems.add(new GameSubsystem(new TTTLibraryImpl()));
+        _server = new Server("/",8080,subsystems);
+        _server.start();
+        Thread.sleep(400);
+    }
     
-    private Runnable startServer=new Runnable() {
-        @Override
-        public void run() {
-            String [] args={"-p", Integer.toString(port), "-d", "/Users"};
-            StartTTTServer.main(args);
-        }
-    };
+    @After
+    public void killServer(){
+        _server.kill();
+    }
     
     @Test
-    public void buildsTTTResponse()
+    public void buildsTTTMoveResponse()
     throws Exception{
-        new Thread(startServer).start();
-        Thread.sleep(400);
         Socket socket=new Socket("localhost",port);
         String board="xoxxox   ";
         char player='o';
@@ -49,8 +59,27 @@ public class IntegrationTest {
         int [] move=new TTTLibraryImpl().getMove(BoardStringParser.stringToBoardVector(board),player,Integer.parseInt(maxdepth));
         String moveString=Integer.toString(move[0])+Integer.toString(move[1]);
         assertTrue(line.indexOf(moveString)>0);
-
     }
-    
+
+    @Test
+    public void buildsWinnerResponse()
+    throws Exception{
+        Socket socket=new Socket("localhost",port);
+        String board="xxxoo o  ";
+        String body=("board="+board);
+        String contentLength=Integer.toString(body.getBytes().length);
+        byte [] requestBytes=("POST /ttt/winner HTTP/1.1\nContent-Length: "+contentLength+"\n\n"+body).getBytes();
+
+        socket.getOutputStream().write(requestBytes);
+
+        BufferedReader reader=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String line="";
+        Thread.sleep(2000);
+        while(reader.ready()){
+            line=reader.readLine();
+        }
+
+        assertTrue(line.indexOf("x")>0);
+    }
     
 }
